4ca7a04ee6e9f144c4f10b5ab03ef909f1b269e1
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-05-31 20:52:30 -0400

    added callback for end on edit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, TextInput, StyleSheet, Button } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-const SearchBar = ({ term, onTermChange }) => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
     <View style={styles.backgroundStyle}>
       <Ionicons name="md-search" style={styles.iconStyle} />
@@ -12,7 +12,8 @@ const SearchBar = ({ term, onTermChange }) => {
       placeholder="Search"
       style={styles.inputStyle}
       value={term}
-      onChangeText={newTerm => onTermChange(newTerm)}
+      onChangeText={onTermChange}
+      onEndEditing={onTermSubmit}
       />
     </View>
   )
