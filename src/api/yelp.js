69c103074fd52c06183f73c0672899c416fbe014
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-06-01 08:25:14 -0400

    fixed bearer typo

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-
+  baseURL: 'https://api.yelp.com/v3/businesses',
+  headers: {
+    Authorization: 'Bearer X4b39-C1_dmvBvydD-NunmaDAbKHWsVO7A_steK0zrvhMefxZw9aG0rhkmtifaOFmFkK4PtXna4uuLR4_vWt44enjQ2W1U33rfiFiHM0Z7eaVw2MQUlsIZgWyYjSXnYx'
+  }
 });
