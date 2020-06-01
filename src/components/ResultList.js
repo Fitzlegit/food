739d2fb19e2b927e9cfb6fa36e2b46d58881ea2a
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-06-01 10:56:14 -0400

    styled margins

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
+import ResultsDetail from './ResultsDetail'
 
 const ResultsList = ({ title, results }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.titleStyle}>{title}</Text>
       <FlatList
         horizontal
         data={results}
         keyExtractor={(result) => result.id}
         renderItem={({ item }) => {
-          return <Text>{item.name}</Text>
+          return <ResultsDetail result={item} />;
         }}
       />
     </View>
@@ -19,9 +20,14 @@ const ResultsList = ({ title, results }) => {
 
 const styles = StyleSheet.create({
   titleStyle: {
+    marginLeft: 20,
     fontSize: 18,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    marginBottom: 5
   },
+  container: {
+    marginBottom: 10
+  }
 });
 
 export default ResultsList;
