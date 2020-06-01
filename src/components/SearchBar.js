09639885fdaa42bd8e3dad07e340cc793b5721f1
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-05-31 20:16:15 -0400

    styled search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,38 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, TextInput, StyleSheet, Button } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 const SearchBar = () => {
   return (
-    <View style={styles.background}>
-      <Ionicons name="md-search" size={30}/>
-      <Text>Search Bar</Text>
+    <View style={styles.backgroundStyle}>
+      <Ionicons name="md-search" style={styles.iconStyle} />
+      <TextInput
+      placeholder="Search"
+      style={styles.inputStyle}
+      />
     </View>
   )
 };
 
 const styles = StyleSheet.create({
-  background: {
-    backgroundColor: '#F0EEEE',
+  backgroundStyle: {
+    marginTop: 15,
+    backgroundColor: '#E8E8E8',
     height: 50,
     borderRadius: 10,
-    marginHorizontal: 20
+    marginHorizontal: 20,
+    flexDirection: 'row',
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18
+  },
+  iconStyle: {
+    fontSize: 30,
+    alignSelf: 'center',
+    marginHorizontal: 15
   }
+
 });
 
 export default SearchBar;
